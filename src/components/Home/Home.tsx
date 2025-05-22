a1894f0cb1ff21ac67791a530e02a0dcddb969f1
Bryan Garcia <bgarci55@depaul.edu>
2025-05-22 00:27:29 -0500

    reformatting, laying down some grid

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,26 @@
 import "./home.css"
 
+
 function Home(){
     return(
         <>
             <body>
                 <main>
-                    <h1 className="welcome"> welcome </h1>
-                    <p className="wip"> work in progress... </p>
+                    <div className="welcomeHolder">
+                        <div className="welcomeDiv">
+                            <p className="motto"> "SIMPLYFING CS" </p>
+                            <h1 className="welcome">Who are we?</h1>
+                            <div className="wip">
+                                <p>We are a student led organization at DePaul University focused on fostering a community through peer-to-peer computer science tutoring open to all majors.</p>
+                            </div>
+                        </div>
+                        <img className="simplylogo" src="src/Assets/IMG_5051.png" width="200" height="200"></img>
+
+                    </div>
+
+                    {/* <div className="space">
+                        <p> exmaple </p>
+                    </div> */}
                 </main>
             </body>
         </>
